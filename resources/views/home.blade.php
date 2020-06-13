ebff7564742dd96eace6a3f597ab6d2271b07ba2
RCRalph <r.chrzanowski.priv@gmail.com>
2020-06-13 19:26:14 +0200

    Changed name to username in registration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,97 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-lg-5 mt-3">
+            <div class="card h-100">
+                <div class="card-header">Account Balance</div>
+                <div class="card-body w-100">
+                    <div class="h2 text-center">
+                        122 133.00 PLN
+                    </div>
+                    <hr>
+                    <div class="h2 text-center">
+                        213.00 USD
+                    </div>
+                    <div class="h2 text-center">
+                        122 133.00 PLN
+                    </div>
+                    <hr>
+                    <div class="h2 text-center">
+                        213.00 USD
+                    </div>
+                    <div class="h2 text-center">
+                        122 133.00 PLN
+                    </div>
+                    <hr>
+                    <div class="h2 text-center">
+                        213.00 USD
+                    </div>
+                    <div class="h2 text-center">
+                        122 133.00 PLN
+                    </div>
+                    <hr>
+                    <div class="h2 text-center">
+                        213.00 USD
+                    </div>
+                    <div class="h2 text-center">
+                        122 133.00 PLN
+                    </div>
+                    <hr>
+                    <div class="h2 text-center">
+                        213.00 USD
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7 mt-3">
+            <div class="card h-100">
+                <div class="card-header">Payment Requests</div>
+                <div class="card-body">
+                    <table class="table table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="text-center">ID</th>
+                                <th scope="col" class="text-center">User</th>
+                                <th scope="col" class="text-center">Amount</th>
+                                <th scope="col" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row" class="text-center">1</th>
+                                <td class="text-center">User 1</td>
+                                <td class="text-center">100.00 EUR</td>
+                                <td><a role="button" class="btn btn-primary btn-sm btn-block" href="#">View request</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" class="text-center">213213213</th>
+                                <td class="text-center">User 132132131</td>
+                                <td class="text-center">12 321 100.00 EUR</td>
+                                <td><a role="button" class="btn btn-primary btn-sm btn-block" href="#">View request</a></td>
+                            </tr>
+                            <tr>
+                                <th scope="row" class="text-center">1</th>
+                                <td class="text-center">User 1</td>
+                                <td class="text-center">100.00 EUR</td>
+                                <td><a role="button" class="btn btn-primary btn-sm btn-block" href="#">View request</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-12 mt-3">
+            <div class="card">
+                <div class="card-header">Recent Transactions</div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--<div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -18,6 +109,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,42 @@
-<!DOCTYPE html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="content">
+                <div class="title m-b-md">
+                    QuickPay
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+<!--<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>QuickPay</title>
 
-        <!-- Fonts -->
+        <!-- Fonts
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
+        <!-- Styles
         <style>
             html, body {
                 background-color: #fff;
@@ -97,4 +124,4 @@
             </div>
         </div>
     </body>
-</html>
+</html>-->
