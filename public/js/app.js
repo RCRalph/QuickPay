aab19a999077d0383ee758b50fbc7a504c8522f4
RCRalph <r.chrzanowski.priv@gmail.com>
2020-07-02 11:42:41 +0200

    Changed bootstap loading

--- public/js/app.js
+++ public/js/app.js
@@ -37250,9 +37250,10 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 
 try {
   window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"];
-  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
   __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
+
+  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 } catch (e) {}
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
