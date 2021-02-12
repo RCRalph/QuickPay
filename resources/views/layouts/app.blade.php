079627a3ca07dbd8762d48d42edb0718066452d9
RCRalph <36704328+RCRalph@users.noreply.github.com>
2021-02-13 00:26:45 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -108,6 +105,8 @@
             @yield('content')
         </main>
     </div>
+	<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 	@yield('script')
 </body>
 </html>
